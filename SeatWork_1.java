4e316e9dfddb17f58cde5c6c41694b8c26a4d3fd
plaknaplak <156560073+plaknaplak@users.noreply.github.com>
2024-01-14 11:46:35 +0800

    Add files via upload

---
+++ SeatWork_1.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class SeatWork_1
+
+    @Override
+    public String toString() {
+        return "SeatWork_1 []";
+    };
+public static;
+       integer
---
+++ demo.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+
+public class demo {
+    public static void main(String[] args) {
+
+        String name;
+        
+     name=   JOptionPane.showInputDialog("enter name");
+
+        JOptionPane.showMessageDialog(null, "name "+ name);
+
+         
+
+
+
+    }
+}
